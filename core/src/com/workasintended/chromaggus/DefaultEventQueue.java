432f11187e6d913e0332108653d75b58f4b5df5b
mazimeng <mazimeng@hotmail.com>
2016-01-20 00:47:27 +0800

    sync

--- core/src/com/workasintended/chromaggus/DefaultEventQueue.java
+++ core/src/com/workasintended/chromaggus/DefaultEventQueue.java
@@ -1,32 +1,30 @@
 package com.workasintended.chromaggus;
 
-import com.workasintended.chromaggus.event.MoveToPositionEvent;
-
 import java.util.HashMap;
 import java.util.LinkedList;
 
 public class DefaultEventQueue implements EventQueue {
 	private LinkedList<Event> events;
-	private HashMap<Class<?>, LinkedList<EventHandler>> handlers;
+	private HashMap<EventName, LinkedList<EventHandler>> handlers;
 	public DefaultEventQueue() {
 		this.events = new LinkedList<Event>();
-		this.handlers = new HashMap<Class<?>, LinkedList<EventHandler>>();
+		this.handlers = new HashMap<EventName, LinkedList<EventHandler>>();
 	}
-	
+
 	public void handle(float delta) {
 		while(!this.events.isEmpty()) {
 			Event event = this.events.pop();
 			LinkedList<EventHandler> handlerList = handlers.get(event.getName());
-			
+
 			if(handlerList == null) continue;
-			
+
 			for(EventHandler handler: handlerList) {
 				handler.handle(event);
 			}
 		}
 	}
-	
-	public void register(Class<Event> eventClass, EventHandler handler) {
+
+	public void register(EventName name, EventHandler handler) {
 //		LinkedList<EventHandler> handlerList = null;
 //		if(!this.handlers.containsKey(name)) {
 //			handlerList = new LinkedList<EventHandler>();
@@ -36,7 +34,7 @@ public class DefaultEventQueue implements EventQueue {
 //
 //		}
 
-		LinkedList<EventHandler> handlerList = this.handlers.get(eventClass);
+		LinkedList<EventHandler> handlerList = this.handlers.get(name);
 		if(handlerList == null) {
 			handlerList = new LinkedList<EventHandler>();
 		}
